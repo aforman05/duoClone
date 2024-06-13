1e8fc9e99834404ae3fd71deb6044c01941db74f
Andrea F <143133024+aforman05@users.noreply.github.com>
2024-06-12 22:15:19 -0500

    Sidebar

--- lingo/app/(main)/layout.tsx
+++ lingo/app/(main)/layout.tsx
@@ -1,3 +1,6 @@
+import { Sidebar } from "@/components/sidebar";
+import { MobileHeader } from "@/components/mobile-header";
+
 type Props = {
     children: React.ReactNode;
 };
@@ -6,9 +9,16 @@ const MainLayout = ({
     children,
 }: Props) => {
     return (
-        <div>
+        <>
+            <MobileHeader />
+            <Sidebar className="hidden lg:flex"/>
+            <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
+            <div
+            className="bg-red-500 h-full">
             {children}
-        </div>
+            </div>
+            </main>
+        </>
     );
 };
 
